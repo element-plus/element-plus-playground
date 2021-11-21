0696d02d1cb1290a53eb0cbf454ad15ab136765f
Kevin <sxzz@sxzz.moe>
2021-11-21 22:48:57 +0800

    feat: add auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// import
+// unplugin-vue-components
 
 export default defineConfig({
-  plugins: [vue()],
-});
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+})
