cd1b62216138a0f1f4107b51c6bc0507e11d72b6
Kevin <sxzz@sxzz.moe>
2022-02-08 08:02:01 +0800

    feat: add default vue app for devtools

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,9 @@ import 'uno.css'
 
 import App from './App.vue'
 
+// @ts-expect-error Custom window property
+window.VUE_DEVTOOLS_CONFIG = {
+  defaultSelectedAppId: 'repl',
+}
+
 createApp(App).mount('#app')
