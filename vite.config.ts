fd238cae179d3e75e3a0585c54d9fc3536626a02
Kevin <sxzz@sxzz.moe>
2022-02-09 17:11:53 +0800

    feat: enable https when dev, closes #142, #148

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
 import Inspect from 'vite-plugin-inspect'
+import Mkcert from 'vite-plugin-mkcert'
 import { getPackageInfo } from 'local-pkg'
 import pkg from './package.json'
 
@@ -24,6 +25,10 @@ export default defineConfig(async () => {
       'import.meta.env.APP_VERSION': JSON.stringify(pkg.version),
       'import.meta.env.REPL_VERSION': JSON.stringify(repl.version),
     },
+    server: {
+      https: true,
+      host: true,
+    },
     plugins: [
       vue({
         reactivityTransform: `${pathSrc}/**/*`,
@@ -40,6 +45,7 @@ export default defineConfig(async () => {
       Unocss({
         presets: [presetUno()],
       }),
+      Mkcert(),
       Inspect(),
     ],
   }
