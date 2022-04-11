9369b8e24e1554552869144a2ad5852aa86f16ba
三咲智子 <sxzz@sxzz.moe>
2022-04-12 04:15:47 +0800

    chore: upgrade deps

--- src/components.d.ts
+++ src/components.d.ts
@@ -2,7 +2,7 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
-declare module 'vue' {
+declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElOption: typeof import('element-plus/es')['ElOption']
