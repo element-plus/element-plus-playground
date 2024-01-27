f7330c98da2cefff8c7cbb5f9708e01de07b0b4c
btea <2356281422@qq.com>
2024-01-27 16:04:52 +0800

    fix: remove https config and remove `local-pkg` (#252)

--- vite.config.ts
+++ vite.config.ts
@@ -8,60 +8,56 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
 import Inspect from 'vite-plugin-inspect'
 import Mkcert from 'vite-plugin-mkcert'
-import { getPackageInfo } from 'local-pkg'
 import pkg from './package.json'
+import replPkg from '@vue/repl/package.json' assert { type: 'json' }
 
 const pathSrc = path.resolve(__dirname, 'src')
 
-export default defineConfig(async () => {
-  const repl = await getPackageInfo('@vue/repl')
-  return {
-    resolve: {
-      alias: {
-        '@': pathSrc,
-      },
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': pathSrc,
     },
-    define: {
-      'import.meta.env.APP_VERSION': JSON.stringify(pkg.version),
-      'import.meta.env.REPL_VERSION': JSON.stringify(repl!.version),
+  },
+  define: {
+    'import.meta.env.APP_VERSION': JSON.stringify(pkg.version),
+    'import.meta.env.REPL_VERSION': JSON.stringify(replPkg.version),
+  },
+  build: {
+    rollupOptions: {
+      external: ['typescript'],
     },
-    build: {
-      rollupOptions: {
-        external: ['typescript'],
-      },
-    },
-    server: {
-      https: true,
-      host: true,
-    },
-    plugins: [
-      vue({
-        script: {
-          defineModel: true,
-          propsDestructure: true,
-          fs: {
-            fileExists: fs.existsSync,
-            readFile: (file) => fs.readFileSync(file, 'utf-8'),
-          },
+  },
+  server: {
+    host: true,
+  },
+  plugins: [
+    vue({
+      script: {
+        defineModel: true,
+        propsDestructure: true,
+        fs: {
+          fileExists: fs.existsSync,
+          readFile: (file) => fs.readFileSync(file, 'utf-8'),
         },
-      }),
-      AutoImport({
-        dirs: [path.resolve(pathSrc, 'composables')],
-        imports: ['vue', '@vueuse/core'],
-        resolvers: [ElementPlusResolver()],
-        dts: path.resolve(pathSrc, 'auto-imports.d.ts'),
-      }),
-      Components({
-        dirs: [path.resolve(pathSrc, 'components')],
-        resolvers: [ElementPlusResolver()],
-        dts: path.resolve(pathSrc, 'components.d.ts'),
-      }),
-      Unocss(),
-      Mkcert(),
-      Inspect(),
-    ],
-    optimizeDeps: {
-      exclude: ['@vue/repl'],
-    },
-  }
+      },
+    }),
+    AutoImport({
+      dirs: [path.resolve(pathSrc, 'composables')],
+      imports: ['vue', '@vueuse/core'],
+      resolvers: [ElementPlusResolver()],
+      dts: path.resolve(pathSrc, 'auto-imports.d.ts'),
+    }),
+    Components({
+      dirs: [path.resolve(pathSrc, 'components')],
+      resolvers: [ElementPlusResolver()],
+      dts: path.resolve(pathSrc, 'components.d.ts'),
+    }),
+    Unocss(),
+    Mkcert(),
+    Inspect(),
+  ],
+  optimizeDeps: {
+    exclude: ['@vue/repl'],
+  },
 })
